52817cf95ea0f8fbffad95e93bbdc4cc327beefc
AimiIsFat <SugarySenpai64@gmail.com>
2022-10-30 06:44:07 -0600

    Setting up Rojo

--- src/ServerStorage/Races/Kemotane/Neko.lua
+++
@@ -1,7 +0,0 @@
-local subtype = {
-	Name = "Neko Type",
-	Description = "Neko Kemotane have cat ears and tails. They tend to be lazier and more mischievious, but also have more stamina and agility.",
-	Traits = {}
-}
-
-return subtype
