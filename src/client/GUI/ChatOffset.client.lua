100f9155fa9968e234a881adcb37028802402c63
AimiIsFat <SugarySenpai64@gmail.com>
2022-11-25 01:08:09 -0700

    Fixed sizing bug

--- src/client/GUI/ChatOffset.client.lua
+++ src/client/GUI/ChatOffset.client.lua
@@ -4,11 +4,13 @@ local chatGui = playerGui:WaitForChild("Chat")
 local chat: Frame = chatGui:WaitForChild("Frame")
 
 local chatFrame1: Frame = chat:WaitForChild("ChannelsBarParentFrame")
-local chatFrame2: Frame = chat:WaitForChild("ChatBarParentFrame")
+local chatFrame2: Frame = chat:WaitForChild("ChatBarParentFrame"):WaitForChild("Frame")
 local chatFrame3: Frame = chat:WaitForChild("ChatChannelParentFrame")
 
+chatFrame2.AutomaticSize = Enum.AutomaticSize.Y
+
 local function update()
-	local offset = chatFrame1.AbsoluteSize.Y + chatFrame2.AbsoluteSize.Y + chatFrame3.AbsoluteSize.Y
+	local offset = chatFrame1.AbsoluteSize.Y + chatFrame2.AbsoluteSize.Y + chatFrame3.AbsoluteSize.Y + 4
 
 	chat.Position = UDim2.new(0, 0, 1, -offset)
 end
