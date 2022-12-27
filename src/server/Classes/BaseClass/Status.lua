da6ad2345aac83757064865978f7fc588139a2c0
AimiIsFat <SugarySenpai64@gmail.com>
2022-12-27 16:09:10 -0700

    Update Status.lua
    Fixed warnings

--- src/server/Classes/BaseClass/Status.lua
+++ src/server/Classes/BaseClass/Status.lua
@@ -1,5 +1,5 @@
-local Knit = require(game:GetService("ReplicatedStorage").Packages.Knit)
-local Signal = require(Knit.Util.Signal)
+--local Knit = require(game:GetService("ReplicatedStorage").Packages.Knit)
+--local Signal = require(Knit.Util.Signal)
 
 local BaseClass = require(script.Parent)
 
@@ -9,33 +9,33 @@ Status.__Replicated = {
 	Vitals = true,
 	Traits = true,
 	Effects = true,
-	Id = true
+	Id = true,
 }
 
 Status.__Defaults = {
 	Vitals = {
 		Health = 100,
 		MaxHealth = 100,
-		
+
 		Hunger = 100,
 		MaxHunger = 100,
-		
+
 		Thirst = 100,
 		MaxThirst = 100,
-		
+
 		Stamina = 100,
 		MaxStamina = 100,
-		
+
 		Mana = 100,
-		MaxMana = 100
+		MaxMana = 100,
 	},
-	
+
 	Traits = {},
-	Effects = {}
+	Effects = {},
 }
 
 function Status:DrainVital(name, n)
-	self.Vitals[name] -= math.clamp(n, 0, self.Vitals["Max"..name]*2)
+	self.Vitals[name] -= math.clamp(n, 0, self.Vitals["Max" .. name] * 2)
 	self.Updated:Fire()
 end
 
