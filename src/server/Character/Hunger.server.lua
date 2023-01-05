9cf64e8d58ccd2f7d0c6f51b621b0f090393bc84
AimiIsFat <SugarySenpai64@gmail.com>
2023-01-05 04:19:22 -0700

    Create Hunger.server.lua

---
+++ src/server/Character/Hunger.server.lua
@@ -0,0 +1,17 @@
+local Players = game:GetService("Players")
+local Knit = require(game:GetService("ReplicatedStorage").Packages.Knit)
+
+Knit.OnStart():await()
+
+local DataManager = Knit.GetService("DataManager")
+
+Players.PlayerAdded:Connect(function(player)
+	while task.wait(1) do
+		if player.Character then
+			local data = DataManager:Get(player)
+			local status = data.Status
+
+			status:DrainVital("Hunger", 1)
+		end
+	end
+end)
